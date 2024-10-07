0cee2e1f94754876991325eab412716bc158bb19
George <socialimp@mail.ru>
2024-10-07 22:22:10 +0300

    :add: start project

---
+++ src/Layout.jsx
@@ -0,0 +1,5 @@
+const Layout = () => {
+    return <div>Hello from Layout component</div>
+}
+
+export default Layout
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Layout from './Layout'
+
+
+createRoot(document.getElementById('root')).render(
+  <div>
+    <h1>Test</h1>
+    <Layout/>
+  </div>
+)
