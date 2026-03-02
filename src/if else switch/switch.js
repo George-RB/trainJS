7f90070e3ec3c8821f664901d892eb2ea0ad5327
George Kartavenko <socialimp@mail.ru>
2026-03-02 21:43:24 +0300

    :feat: swicth

---
+++ src/if else switch/switch.js
@@ -0,0 +1,15 @@
+let a = 10, b = 15, c = 20;
+let sum = c - b;
+
+switch (sum) {
+    case 1: console.log('1');
+    break;
+    case 2: console.log(24);
+    break;
+    case 3: console.log('threw')
+    break;
+    case 5: console.log("Five");
+    break;
+    default: console.log('1 throw 2 you')
+}
+
